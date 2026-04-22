159ae7c56d4f57b3bbe94151b279d5422eb19a30
DaniloIB <ibaniez.danilo@gmail.com>
2026-04-22 00:38:55 -0300

    Se crean las clases Marca, Categoria y Articulo

---
+++ tp-winform-equipo-6A/Articulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_winform_equipo_6A
+{
+    class Articulo
+    {
+        public int CodArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public decimal Precio { get; set; }
+        public List<Imagen> Imagenes { get; set; }
+    }
+}
---
+++ tp-winform-equipo-6A/Marca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_winform_equipo_6A
+{
+    class Marca
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+
+}

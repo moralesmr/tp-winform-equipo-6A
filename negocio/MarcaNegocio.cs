049a59801340c84e565a6bea679f6da81733f672
moralesmr <rosamorales9630@gmail.com>
2026-04-25 02:19:20 -0300

    creacion de archivo marca de negocio

---
+++ negocio/MarcaNegocio.cs
@@ -0,0 +1,43 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace negocio
+{
+    public class MarcaNegocio
+    {
+        public List<Marca> Listar() {
+
+            List<Marca> marca = new List<Marca>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setConsulta("select Id,Descripcion from MARCAS");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    lista.Add(aux);
+
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally {
+                datos.cerrarConexion();
+            }
+        }
+
+    }
+}

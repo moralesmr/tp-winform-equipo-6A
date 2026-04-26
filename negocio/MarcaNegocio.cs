d3ddaea7d954df30f2605a00b65d4173e1e6a6f8
moralesmr <rosamorales9630@gmail.com>
2026-04-26 11:58:42 -0300

    agregado de logicametodos

--- negocio/MarcaNegocio.cs
+++ negocio/MarcaNegocio.cs
@@ -17,27 +17,95 @@ namespace negocio
 
             try
             {
-                datos.setConsulta("select Id,Descripcion from MARCAS");
+                datos.setConsulta("SELECT Id, Descripcion FROM MARCAS");
                 datos.ejecutarLectura();
+
                 while (datos.Lector.Read())
                 {
                     Marca aux = new Marca();
                     aux.Id = (int)datos.Lector["Id"];
                     aux.Descripcion = (string)datos.Lector["Descripcion"];
-                    lista.Add(aux);
 
+                    marca.Add(aux);
                 }
-                return lista;
             }
             catch (Exception ex)
             {
-
                 throw ex;
             }
-            finally {
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+            return marca;
+        }
+
+
+        public void Agregar(Marca nueva)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setConsulta("INSERT INTO MARCAS (Descripcion) VALUES (@desc)");
+                datos.setearParametros("@desc", nueva.Descripcion);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+
+        }
+
+        public void Modificar(Marca marca)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setConsulta("UPDATE MARCAS SET Descripcion = @desc WHERE Id = @id");
+                datos.setearParametros("@desc", marca.Descripcion);
+                datos.setearParametros("@id", marca.Id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
                 datos.cerrarConexion();
             }
         }
+
+
+        public void Eliminar(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setConsulta("DELETE FROM MARCAS WHERE Id = @id");
+                datos.setearParametros("@id", id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
 
     }
 }

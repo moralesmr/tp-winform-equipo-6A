0b398d3ce781c72bfa877c6907b790ec102c95f8
moralesmr <rosamorales9630@gmail.com>
2026-04-26 21:01:21 -0300

    cambios formulario administrar categorias

--- tp-winform-equipo-6A/frmAdmCategorias.Designer.cs
+++ tp-winform-equipo-6A/frmAdmCategorias.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.listBoxCat = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(558, 404);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 0;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBoxCat
+            // 
+            this.listBoxCat.FormattingEnabled = true;
+            this.listBoxCat.Location = new System.Drawing.Point(52, 30);
+            this.listBoxCat.Name = "listBoxCat";
+            this.listBoxCat.Size = new System.Drawing.Size(430, 342);
+            this.listBoxCat.TabIndex = 1;
+            // 
+            // frmAdmCategorias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmAdmCategorias";
+            this.Controls.Add(this.listBoxCat);
+            this.Controls.Add(this.btnCerrar);
+            this.Name = "frmAdmCategorias";
+            this.Text = "Administrar Categorias";
+            this.Load += new System.EventHandler(this.frmAdmCategorias_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.ListBox listBoxCat;
     }
 }

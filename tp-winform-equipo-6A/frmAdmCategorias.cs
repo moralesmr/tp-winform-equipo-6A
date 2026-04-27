432d4427c3a33a5130442c6d993fdf88b24e8bc0
maria rosa morales <156377897+moralesmr@users.noreply.github.com>
2026-04-26 21:21:07 -0300

    Implement cargar method for category loading
    Added cargar method to load categories into listBoxCat.

--- tp-winform-equipo-6A/frmAdmCategorias.cs
+++ tp-winform-equipo-6A/frmAdmCategorias.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using dominio;
+using negocio;
 
 namespace tp_winform_equipo_6A
 {
@@ -19,8 +21,24 @@ namespace tp_winform_equipo_6A
 
         private void frmAdmCategorias_Load(object sender, EventArgs e)
         {
-
+            cargar();            
         }
+
+          private void cargar()
+          {
+              try
+              {
+                  CategoriaNegocio negocio = new CategoriaNegocio();
+                  listaCategorias = negocio.listar();
+        
+                  listBoxCat.DataSource = null;
+                  listBoxCat.DataSource = listaCategorias;
+              }
+              catch (Exception ex)
+              {
+                  MessageBox.Show("error en la carga: " + ex.Message);
+              }
+          }
 
         private void btnCerrar_Click(object sender, EventArgs e)
         {
